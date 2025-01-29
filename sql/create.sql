ad3f1be04b5f6cab5e331b6403af60b766722816
Silverfeelin <Silverfeelin@gmail.com>
2025-01-29 17:31:37 +0100

    Seems to work locally

---
+++ sql/create.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS markers (
+  [id] integer PRIMARY KEY AUTOINCREMENT,
+  [createdOn] DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  [userId] char(20) NOT NULL,
+  [username] varchar(32) NOT NULL,
+  [epoch] int NOT NULL,
+	[lat] double NOT NULL,
+	[lng] double NOT NULL,
+  [size] int NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_markers_epoch ON markers(epoch);
--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,15 @@ interface ISession {
 
 interface IMarker {
   epoch: number;
-  pos: [number, number];
+  lat: number;
+	lng: number;
+	size: number;
+}
+
+interface IMessage {
+	type: 'marker' | 'validation';
+	marker?: IMarker;
+	message?: string;
 }
 
 export class SkyGameDyeServer extends DurableObject<Env> {
@@ -31,29 +39,64 @@ export class SkyGameDyeServer extends DurableObject<Env> {
 
     this.sessions = new Map();
     this.markers = [];
+
+    // Load sessions.
+    this.ctx.getWebSockets().forEach(ws => {
+      const session: ISession = ws.deserializeAttachment();
+      this.sessions.set(ws, session);
+    });
+
+    // Load data from storage.
+    this.ctx.blockConcurrencyWhile(async () => {
+      await this.load();
+    });
+  }
+
+  async load(): Promise<void> {
+		const date = new Date();
+		date.setUTCMinutes(0, 0, 0);
+		const epoch = date.getTime();
+
+    const data = await this.env.DB.prepare(`SELECT * FROM markers WHERE epoch = ?;`).bind(epoch).all();
+		this.markers = data.results.map((row): IMarker => ({
+			epoch: row.epoch as number,
+			lat: row.lat as number,
+			lng: row.lng as number,
+			size: row.size as number
+		}));
   }
 
   async fetch(request: Request): Promise<Response> {
 		// Check request for websocket upgrade.
     const url = new URL(request.url);
-    if (url.pathname !== '/ws') { return new Response('Not found', { status: 404 }); }
+    if (url.pathname !== '/api/ws') { return new Response('Not found', { status: 404 }); }
 
     const upgradeHeader = request.headers.get('Upgrade');
     if (upgradeHeader !== 'websocket') { return new Response('Not a websocket request', { status: 400 }); }
 
 		// Check Authorization.
-    const authHeader = request.headers.get('Authorization');
+		const cookies = request.headers.get('Cookie');
+    let authHeader = cookies?.split(';').find(cookie => cookie.startsWith('Authorization='))?.split('=')[1];
+		if (authHeader) { authHeader = decodeURIComponent(authHeader); }
     if (!authHeader) { return new Response('Missing Authorization header', { status: 401 }); }
+
     const [userId, username] = await this.getUserIdAsync(authHeader);
+    if (!userId || !username) { return new Response('Failed to fetch user data', { status: 401 }); }
+
+    const session: ISession = {
+			userId, username
+		};
 
 		// Create websocket connection.
     const pair = new WebSocketPair();
     this.ctx.acceptWebSocket(pair[0]);
 
-    const session: ISession = {
-			userId, username
-		};
+    // Save the user ID and username in the session.
+    pair[0].serializeAttachment(session);
     this.sessions.set(pair[0], session);
+
+		// Send marker history.
+		this.sendHistory(pair[0]);
 
     return new Response(null, {
       status: 101,
@@ -83,15 +126,64 @@ export class SkyGameDyeServer extends DurableObject<Env> {
   }
 
   async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer): Promise<void> {
+		const close = (reason: string) => {
+			ws.close(1008, reason);
+			this.sessions.delete(ws);
+		};
+
+		// Find session
     const session = this.sessions.get(ws);
-    if (!session) { return; }
+    if (!session) {
+			return close('Session not found.');
+		}
 
-    const obj = JSON.parse(message.toString());
+		let marker: IMarker;
 
-		throw new Error('TODO');
-		//this.env.DB.prepare()
+		try {
+			// Read marker
+			const obj = JSON.parse(message.toString());
+			marker = { epoch: 0, lat: obj.lat, lng: obj.lng, size: obj.size };
+			console.log('Received marker:', marker);
 
-    await this.broadcast(`Someone said: ${obj}`);
+			// Validate marker
+			if (isNaN(marker.size) || marker.size < 1 || marker.size > 3) {
+				return close('Invalid size.');
+			}
+
+			// Validate location
+			if (isNaN(marker.lat) || isNaN(marker.lng)) {
+				return close('Invalid location.');
+			}
+
+			// Validate time
+			const date = new Date();
+			if (date.getUTCMinutes() >= 55) {
+				this.sendMessage(ws, { type: 'validation', message: 'Please wait until the next hour to place a marker.' });
+				return;
+			}
+
+			// Set epoch
+			date.setUTCMinutes(0, 0, 0);
+			marker.epoch = date.getTime();
+
+			const query = `
+				INSERT INTO markers (userId, username, epoch, lat, lng, size)
+				VALUES (?, ?, ?, ?, ?, ?);
+			`;
+
+			const result = await this.env.DB.prepare(query).bind(
+				session.userId, session.username, marker.epoch, marker.lat, marker.lng, marker.size
+			).run();
+			console.log('Saved to DB:', result.meta?.rows_written);
+		} catch (e) {
+			console.error(e);
+			ws.close(1008, 'Failed to parse message.');
+			this.sessions.delete(ws);
+			return;
+		}
+
+		this.markers.push(marker);
+    await this.broadcast({ type: 'marker', marker });
   }
 
   async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean): Promise<void> {
@@ -103,10 +195,21 @@ export class SkyGameDyeServer extends DurableObject<Env> {
     this.sessions.delete(ws);
   }
 
-  async broadcast(message: string): Promise<void> {
+	/** Sends the marker history to a socket. */
+  async sendHistory(ws: WebSocket): Promise<void> {
+    this.clearOldMarkers();
+
+		console.log('Sending markers:', this.markers.length);
+    for (const marker of this.markers) {
+			this.sendMessage(ws, { type: 'marker', marker });
+    }
+  }
+
+	/** Broadcasts a message to all sockets. */
+  async broadcast(message: IMessage): Promise<void> {
     this.sessions.forEach((session, ws) => {
       try {
-        ws.send(message);
+        this.sendMessage(ws, message);
       } catch (e) {
         console.error(e);
         this.sessions.delete(ws);
@@ -114,20 +217,18 @@ export class SkyGameDyeServer extends DurableObject<Env> {
     });
   }
 
-  async sendHistory(ws: WebSocket): Promise<void> {
-    this.clearOldMarkers();
-
-    for (const marker of this.markers) {
-      ws.send(JSON.stringify(marker));
-    }
-  }
+	/** Sends a message to a socket. */
+	async sendMessage(ws: WebSocket, message: IMessage): Promise<void> {
+		ws.send(JSON.stringify(message));
+	}
 
   /** Clears any markers that are no longer relevant. */
   clearOldMarkers(): void {
     const date = new Date();
     date.setUTCMinutes(0, 0, 0);
     const epoch = date.getTime();
-
+		const length = this.markers.length;
     this.markers = this.markers.filter(marker => marker.epoch >= epoch);
+		console.log('Cleared old markers:', length - this.markers.length);
   }
 }
